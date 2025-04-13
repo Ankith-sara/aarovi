8034b566a7d657ccd630cdf247bd1fe209db9feb
Ankith Kumar Sara <ankithkumar.sara_2026@woxsen.edu.in>
2025-04-13 11:44:55 +0530

    Inital commit

--- frontend/src/components/NewsletterBox.jsx
+++ frontend/src/components/NewsletterBox.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { toast } from 'react-toastify';
 
 const NewsletterBox = () => {
+  const [email, setEmail] = useState('');
+
   const onSubmitHandler = (event) => {
     event.preventDefault();
-    alert('Thank you for subscribing!');
+    toast('Thank you for subscribing!');
+    setEmail("");
   };
 
   return (
@@ -16,7 +20,7 @@ const NewsletterBox = () => {
         By subscribing, you agree to our <span className="text-text hover:text-secondary underline cursor-pointer">Privacy Policy</span>.
       </p>
       <form onSubmit={onSubmitHandler} className="flex flex-row sm:flex-row items-center justify-center gap-2">
-        <input type="email" placeholder="Enter your email address" className="w-full sm:w-2/3 md:w-1/2 p-3 rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-secondary text-gray-700" required />
+        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Enter your email address" className="w-full sm:w-2/3 md:w-1/2 p-3 rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-secondary text-gray-700" required />
         <button type="submit" className="bg-primary border-2 border-secondary text-secondary font-medium text-sm px-6 py-3 rounded-md hover:bg-secondary hover:text-primary transition-all duration-300">
           Subscribe
         </button>
